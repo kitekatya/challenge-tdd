c6a7a720702457ab2b2f915baaa8b2b34398dfd7
kitekatya <prostopostya221@yandex.ru>
2023-11-06 12:05:14 +0500

    Try solve

--- App/PostfixCalculator.cs
+++ App/PostfixCalculator.cs
@@ -8,7 +8,65 @@ namespace App
     {
         public static string Calculate(string postfixExpression)
         {
-            throw new NotImplementedException();
+            if (postfixExpression == null)
+                throw new FormatException();
+            
+            var stringNew = postfixExpression.Split().ToList();
+            var result = 0.0;
+            // это не будет сдаваться....... :(
+            //:(((((
+            //for (int i = 0; i < stringNew.Count - 2; i ++)
+            while (stringNew.Count != 1) 
+            {
+                if (stringNew.Count == 2)
+                    throw new FormatException();
+                if (IsDigit(stringNew[0]) && IsDigit(stringNew[1])) 
+                {
+                    if (stringNew[2] == "+") 
+                    {
+                        result = Double.Parse(stringNew[0]) + Double.Parse(stringNew[1]);
+                    }
+                    else if (stringNew[2] == "-") 
+                    {
+                        result = Double.Parse(stringNew[0]) - Double.Parse(stringNew[1]);
+                    }
+                    else if (stringNew[2] == "*") 
+                    {
+                        result = Double.Parse(stringNew[0]) * Double.Parse(stringNew[1]);
+                    }
+                    else 
+                    {
+                        throw new FormatException();
+                    }
+                    stringNew.RemoveAt(0);
+                    stringNew.RemoveAt(0);
+                    stringNew[0] = result.ToString();
+                }
+                else throw new FormatException();
+            }
+            
+            if (postfixExpression == "") 
+            {
+                return "0";
+            }
+            if (IsDigit(stringNew[0]))
+                return stringNew[0];
+            else throw new FormatException();
+            
+        }
+        
+        private static bool IsDigit(string str)
+        {
+            var isDigit = true;
+            var i = 0;
+            if (str[0] == '-') i = 1;
+            for (; i < str.Length; i ++)
+            {
+                if (!Char.IsDigit(str[i]))
+                    return false;
+            }
+
+            return isDigit;
         }
     }
 }
